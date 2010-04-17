56ccfbc6d0003a0603d01cc7baf7bb8a0604625e
Mats Larsen <Alderney87@gmail.com>
2010-04-17 23:03:22 +0200

    hashmap hips

---
+++ Foundation/sas_database.java
@@ -0,0 +1,272 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Foundation;
+
+/**
+ *
+ * @author Mats l
+ */
+import java.sql.*;
+import java.util.ArrayList;
+import domain.*;
+
+public class sas_database {
+
+    private static Connection con;
+    private static String url = "jdbc:mysql://" + "localhost" + ":3306/" + "sas";
+    private static String databaseUser = "root";
+    private static String password = "abc";
+    private int ship_id;
+    private String Name, Captain, Totalvolume, Totalweight, Destination, Dato;
+    private ArrayList<String> DestinationList = new ArrayList<String>();
+    private ArrayList<String> DatoList = new ArrayList<String>();
+
+    public sas_database() {
+    }
+
+    public void createDatabase() {
+//Finder Driveren her
+	try {
+	    Class.forName("com.mysql.jdbc.Driver");
+	    System.out.println("MySQL Driver Found");
+	} catch (java.lang.ClassNotFoundException e) {
+	    System.out.println("MySQL JDBC Driver not found ... ");
+	}
+
+	//Kontakter MySQL databasen her
+	try {
+
+	    con = DriverManager.getConnection(url, databaseUser, password);
+
+	    System.out.println("Connection established to " + url + "...");
+	} catch (java.sql.SQLException e) {
+	    System.out.println("Connection couldn't be established to " + url + e.toString());
+	}
+
+	// create table all ship
+
+	try {
+	    Statement statement = con.createStatement();
+	    String dropString = "DROP TABLE allship;";
+	    statement.executeUpdate(dropString);
+	} catch (SQLException e) {
+	    System.err.println("Got an exception! ");
+	    System.err.println(e.getMessage());
+	}
+
+	try {
+	    Statement statement = con.createStatement();
+	    String createString = "CREATE TABLE allship (ship_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,"
+		    + "Name VARCHAR(100)," + "Captain VARCHAR(100)," + "Totalvolume VARCHAR(100)," + "Totalweight VARCHAR(100));";
+	    statement.executeUpdate(createString);
+	} catch (SQLException e) {
+	    System.err.println("Got an exception! ved createallship ");
+	    System.err.println(e.getMessage());
+	}
+
+	try {
+	    Statement statement = con.createStatement();
+	    String insertString = "INSERT INTO allship (Name, Captain, Totalvolume, Totalweight) VALUES ('god vind','Dan nyguen','10000 m3', ' 8 tons');";
+	    statement.executeUpdate(insertString);
+	    insertString = "INSERT INTO allship (Name, Captain, Totalvolume, Totalweight) VALUES ('I ser mig ikke', 'Stefan larsen', '12000 m3', ' 10 tons');";
+	    statement.executeUpdate(insertString);
+	} catch (SQLException e) {
+	    System.err.println("Got an exception! ved iinsert ");
+	    System.err.println(e.getMessage());
+	}
+
+	try {
+	    Statement statement = con.createStatement();
+	    String dropString = "DROP TABLE 1_ship;";
+	    statement.executeUpdate(dropString);
+	} catch (SQLException e) {
+	    System.err.println("Got an exception! ");
+	    System.err.println(e.getMessage());
+	}
+
+	try {
+	    Statement statement = con.createStatement();
+	    String createString = "CREATE TABLE 1_ship (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,"
+		    + "Destination VARCHAR(100), Dato VARCHAR(100));";
+	    statement.executeUpdate(createString);
+	} catch (SQLException e) {
+	    System.err.println("Got an exception! ved create ");
+	    System.err.println(e.getMessage());
+	}
+
+
+	try {
+	    Statement statement = con.createStatement();
+	    String insertString = "INSERT INTO 1_ship (Destination, Dato) VALUES ('Odense','15-04-2010');";
+	    statement.executeUpdate(insertString);
+	    insertString = "INSERT INTO 1_ship (Destination, Dato) VALUES ('Hamburg', '20-15-2010');";
+	    statement.executeUpdate(insertString);
+	} catch (SQLException e) {
+	    System.err.println("Got an exception! ved insert ");
+	    System.err.println(e.getMessage());
+	}
+
+
+	//Create table called 2_ship
+
+	try {
+	    Statement statement = con.createStatement();
+	    String dropString = "DROP TABLE 2_ship;";
+	    statement.executeUpdate(dropString);
+	} catch (SQLException e) {
+	    System.err.println("Got an exception! ");
+	    System.err.println(e.getMessage());
+	}
+
+	try {
+	    Statement statement = con.createStatement();
+	    String createString = "CREATE TABLE 2_ship (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,"
+		    + "Destination VARCHAR(100), Dato VARCHAR(100));";
+	    statement.executeUpdate(createString);
+	} catch (SQLException e) {
+	    System.err.println("Got an exception! ved create ");
+	    System.err.println(e.getMessage());
+	}
+
+	try {
+	    Statement statement = con.createStatement();
+	    String insertString = "INSERT INTO 2_ship (Destination, Dato) VALUES ('Odense','17-04-2010');";
+	    statement.executeUpdate(insertString);
+	    insertString = "INSERT INTO 2_ship (Destination, Dato) VALUES ('Amsterdam', '25-15-2010');";
+	    statement.executeUpdate(insertString);
+	} catch (SQLException e) {
+	    System.err.println("Got an exception! ved insert ");
+	    System.err.println(e.getMessage());
+
+	}
+    }
+
+    public ArrayList connectToDatabase_allship() throws SQLException {
+
+	ArrayList<String> allshipList = new ArrayList<String>();
+
+	//Finder Driveren her
+	try {
+	    Class.forName("com.mysql.jdbc.Driver");
+	    System.out.println("MySQL Driver Found");
+	} catch (java.lang.ClassNotFoundException e) {
+	    System.out.println("MySQL JDBC Driver not found ... ");
+	}
+
+	//Kontakter MySQL databasen her
+	try {
+
+	    con = DriverManager.getConnection(url, databaseUser, password);
+
+	    System.out.println("Connection established to " + url + "...");
+	} catch (java.sql.SQLException e) {
+	    System.out.println("Connection couldn't be established to " + url + e.toString());
+	}
+
+	try {
+	    String sqlStmt = "SELECT ship_id,Name,Captain, Totalvolume, Totalweight  FROM allship";
+	    Statement stmt;
+	    stmt = con.createStatement();
+	    ResultSet resultSet;
+	    resultSet = stmt.executeQuery(sqlStmt);
+
+
+	    while (resultSet.next()) {
+		Name = resultSet.getString("Name");
+		Captain = resultSet.getString("Captain");
+		ship_id = resultSet.getInt("ship_id");
+		Totalvolume = resultSet.getString("Totalvolume");
+		Totalweight = resultSet.getString("Totalweight");
+		System.out.println("allship [ship_id, Name, Captian]= " + "(" + ship_id + ")" + Name + Captain + Totalvolume + Totalweight);
+		allshipList.add(Integer.toString(ship_id));
+	    }
+	    resultSet.close();
+	} catch (SQLException e) {
+	    System.out.println("Error executing sql statement");
+	}
+	return allshipList;
+
+    }
+
+    public void getShipInfo(int ship_id) {
+
+	ArrayList<String> DestinationList = new ArrayList<String>();
+	ArrayList<String> DatoList = new ArrayList<String>();
+	ConnectToSas();
+	String ship__id = Integer.toString(ship_id);
+	try {
+	    String sqlStmt = "SELECT Destination, Dato  FROM " + ship__id + "_ship";
+	    Statement stmt;
+	    stmt = con.createStatement();
+	    ResultSet resultSet;
+	    resultSet = stmt.executeQuery(sqlStmt);
+
+
+	    while (resultSet.next()) {
+		Destination = resultSet.getString("Destination");
+		Dato = resultSet.getString("Dato");
+
+		System.out.println("1_ship [ Destination, Dato]= " + Destination + Dato);
+
+		DestinationList.add(Destination);
+		DatoList.add(Dato);
+	    }
+	    resultSet.close();
+	} catch (SQLException e) {
+	    System.out.println("Error executing sql statement");
+	}
+
+    }
+
+    public void ConnectToSas() {
+	//Finder Driveren her
+	try {
+	    Class.forName("com.mysql.jdbc.Driver");
+	    System.out.println("MySQL Driver Found");
+	} catch (java.lang.ClassNotFoundException e) {
+	    System.out.println("MySQL JDBC Driver not found ... ");
+	}
+
+	//Kontakter MySQL databasen her
+	try {
+
+	    con = DriverManager.getConnection(url, databaseUser, password);
+
+	    System.out.println("Connection established to " + url + "...");
+	} catch (java.sql.SQLException e) {
+	    System.out.println("Connection couldn't be established to " + url + e.toString());
+	}
+    }
+
+    public int getShipid() {
+	return ship_id;
+    }
+
+    public String getName() {
+	return Name;
+    }
+
+    public String getCaptain() {
+	return Captain;
+    }
+
+    public String getTotalvolume() {
+	return Totalvolume;
+    }
+
+    public String getTotalweight() {
+	return Totalweight;
+    }
+
+    public ArrayList getDestinationList() {
+	return DestinationList;
+    }
+
+
+    public ArrayList getDatoList() {
+	return DatoList;
+    }
+}
+
--- domain/Sas.java
+++ domain/Sas.java
@@ -2,8 +2,10 @@ package domain;
 
 import java.util.*;
 
-import java.sql.*;
+
 import java.util.ArrayList;
+import Foundation.*;
+import java.sql.SQLException;
 
 public class Sas {
 
@@ -20,11 +22,10 @@ public class Sas {
     private User currentUser;
     //Brugeren som er logget ind
     private Map<String, Ship> ships = new HashMap<String, Ship>();
-    private static Connection con;
-    private static String url = "jdbc:mysql://" + "localhost" + ":3306/" + "sas";
-    private static String databaseUser = "root";
-    private static String password = "abc";
-    ArrayList<String> clientChoose = new ArrayList<String>();
+   
+    private ArrayList<String> dates = new ArrayList<String>();
+    private ArrayList<String> clientChoose = new ArrayList<String>();
+
 
     public Sas(User Admin) {
 	this.Admin = Admin;
@@ -33,34 +34,81 @@ public class Sas {
 
     public void setUp() throws Exception {
 	RunServer runS = new RunServer();
-	clientChoose = runS.runServer();
-	//String currentMetode =clientChoose.substring(0,1);
-	//if (currentMetode.equals("1"))   {
+
+	clientChoose = runS.getClientChoose();
+
+	ArrayList ab = new ArrayList();
+	ab.add("asdsad");
+	ab.add("sdsdsssss");
+	ab = clientChoose;
+	try {
+	    System.out.println((ab.get(1)));
+	} catch (IndexOutOfBoundsException e) {
+	    System.out.print("fejl");
+	}
+	clientChoose.clear();
+
+	clientChoose.add("1");
+	clientChoose.add("Odense");
+	clientChoose.add("Amsterdam");
+	clientChoose.add("25-04-2010");
+	clientChoose.add("4000");
+	clientChoose.add("800");
 
 
-	// placeOrder(clientChoose);
-	// System.out.println("qwerrtt");
 
-	System.out.println("god stil ");
+	if (clientChoose.get(0).equals("1")) {
+	    dates = placeOrder();
+	    System.out.print("virker");
+	}
+
+
+
+	//int sd = 1;
+	//System.out.println((String)clientChoose.get(sd));
+
     }
 
-    public String placeOrder(String endLoc, String startLoc, String endDate, 
-	    String volume, String weight) {
+    public ArrayList placeOrder() throws SQLException {
+
+	ArrayList<String> allshipList = new ArrayList<String>();
+	sas_database database = new sas_database();
+
+	allshipList =(database.connectToDatabase_allship());
+
+	   for (int i = 0; i < allshipList.size(); i++) {
+	       database.getShipInfo(Integer.parseInt(allshipList.get(i)));
+	       ship = new Ship(database.getShipid(), database.getName(), database.getCaptain(), database.getTotalvolume(), database.getTotalweight(), database.getDestinationList(), database.getDatoList());
+		ships.put(allshipList.get(i), ship);
+		System.out.println("gentagelse");
+		System.out.println(ship.toString());
+	   }
+
 
 	this.endLoc = endLoc;
 	this.startLoc = startLoc;
 	this.endDate = endDate;
 	this.volume = volume;
 	this.weight = weight;
-	if (ship.availCargo(volume, weight) == true) {
-	}
-
-	return totalPrice = calcPrice(volume, weight, startLoc, endLoc);
-
-
-	//	ArrayList<String> shipsList = new ArrayList<String >( ) ;	
-	//	shipsList.add(ships.get("1").toString());
+	//if (ship.availCargo(volume, weight) == true) {
+	return clientChoose;
     }
+
+    //return totalPrice = calcPrice(volume, weight, startLoc, endLoc);
+    //	ArrayList<String> shipsList = new ArrayList<String >( ) ;
+    //	shipsList.add(ships.get("1").toString());
+
+    
+
+
+
+
+
+
+
+
+
+
 
     public String calcPrice(String volume, String weight, String startLoc,
 	    String endLoc) {
@@ -109,155 +157,24 @@ public class Sas {
     }
      *
      */
- 
-
-    public void seeShipInfo(String shipID) {
-	Ship currentship = null;
-
-	Ship s1 = new Ship("s1", "1000", "45000");
-	Ship s2 = new Ship("s2", "2000", "30000");
-	//tilføj test skibe
-
-	ships.put("s1", s1);
-	ships.put("s2", s2);
-	//lægger skibe i hashmap
-
-	System.out.println(findShip(shipID));
-	//find skib frem
-
-    }
-
     private Ship findShip(String ShipID) {
 	return ships.get(ShipID);
     }
 
-    public void createDatabase() {
-//Finder Driveren her
-	try {
-	    Class.forName("com.mysql.jdbc.Driver");
-	    System.out.println("MySQL Driver Found");
-	} catch (java.lang.ClassNotFoundException e) {
-	    System.out.println("MySQL JDBC Driver not found ... ");
-	}
-
-	//Kontakter MySQL databasen her
-	try {
-
-	    con = DriverManager.getConnection(url, databaseUser, password);
-
-	    System.out.println("Connection established to " + url + "...");
-	} catch (java.sql.SQLException e) {
-	    System.out.println("Connection couldn't be established to " + url + e.toString());
-	}
-
-	// create table all ship
-
-	try {
-	    Statement statement = con.createStatement();
-	    String dropString = "DROP TABLE allship;";
-	    statement.executeUpdate(dropString);
-	} catch (SQLException e) {
-	    System.err.println("Got an exception! ");
-	    System.err.println(e.getMessage());
-	}
-
-	try {
-	    Statement statement = con.createStatement();
-	    String createString = "CREATE TABLE allship (ship_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,"
-		    + "Name VARCHAR(100), Captain VARCHAR(100));";
-	    statement.executeUpdate(createString);
-	} catch (SQLException e) {
-	    System.err.println("Got an exception! ved createallship ");
-	    System.err.println(e.getMessage());
-	}
-
-	try {
-	    Statement statement = con.createStatement();
-	    String insertString = "INSERT INTO allship (Name, Captain) VALUES ('god vind','Dan nyguen');";
-	    statement.executeUpdate(insertString);
-	    insertString = "INSERT INTO allship (Name, Captain) VALUES ('I ser mig ikke', 'Stefan larsen');";
-	    statement.executeUpdate(insertString);
-	} catch (SQLException e) {
-	    System.err.println("Got an exception! ved insert ");
-	    System.err.println(e.getMessage());
-	}
-
-	try {
-	    Statement statement = con.createStatement();
-	    String dropString = "DROP TABLE 1_ship;";
-	    statement.executeUpdate(dropString);
-	} catch (SQLException e) {
-	    System.err.println("Got an exception! ");
-	    System.err.println(e.getMessage());
-	}
-
-	try {
-	    Statement statement = con.createStatement();
-	    String createString = "CREATE TABLE 1_ship (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,"
-		    + "Destination VARCHAR(100), Dato VARCHAR(100));";
-	    statement.executeUpdate(createString);
-	} catch (SQLException e) {
-	    System.err.println("Got an exception! ved create ");
-	    System.err.println(e.getMessage());
-	}
-
-
-	try {
-	    Statement statement = con.createStatement();
-	    String insertString = "INSERT INTO 1_ship (Destination, Dato) VALUES ('Odense','15-04-2010');";
-	    statement.executeUpdate(insertString);
-	    insertString = "INSERT INTO 1_ship (Destination, Dato) VALUES ('Hamburg', '20-15-2010');";
-	    statement.executeUpdate(insertString);
-	} catch (SQLException e) {
-	    System.err.println("Got an exception! ved insert ");
-	    System.err.println(e.getMessage());
-	}
-
-
-	//Create table called 2_ship
-
-	try {
-	    Statement statement = con.createStatement();
-	    String dropString = "DROP TABLE 2_ship;";
-	    statement.executeUpdate(dropString);
-	} catch (SQLException e) {
-	    System.err.println("Got an exception! ");
-	    System.err.println(e.getMessage());
-	}
-
-	try {
-	    Statement statement = con.createStatement();
-	    String createString = "CREATE TABLE 2_ship (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,"
-		    + "Destination VARCHAR(100), Dato VARCHAR(100));";
-	    statement.executeUpdate(createString);
-	} catch (SQLException e) {
-	    System.err.println("Got an exception! ved create ");
-	    System.err.println(e.getMessage());
-	}
-    }
-
+   
 
     public void addShip(String shipID, Ship ship) {
 	ships.put(shipID, ship);
 
-	try
-	 {
-	    Statement statement = con.createStatement();
-	    String insertString = "INSERT INTO 2_ship (Destination, Dato) VALUES ('Odense','17-04-2010');";
-	    statement.executeUpdate(insertString);
-	    insertString = "INSERT INTO 2_ship (Destination, Dato) VALUES ('Amsterdam', '25-15-2010');";
-	    statement.executeUpdate(insertString);
-	} catch (SQLException e) {
-	    System.err.println("Got an exception! ved insert ");
-	    System.err.println(e.getMessage());
-
-	}
+	
 
 
 
 
 
     }
+
+    
 }
 
 
