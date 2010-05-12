02cb9fc165e21ff8a091d6b3aa8d8eec9e9d42a7
unknown <Administrator@.(none)>
2010-05-12 10:31:38 +0200

    presentationslag lagt ind

---
+++ presentation/mappanel/MapAquaintance.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package presentation.mappanel;
+
+
+import java.util.Hashtable;
+import java.util.Map;
+
+/**
+ *
+ * @author Soren V. Jorgensen
+ */
+public class MapAquaintance {
+
+    private static int gpsNr = 1;
+
+    public static Map<Integer, Double> longditude =
+	    new Hashtable<Integer, Double>(50);
+    public static Map<Integer, Double> latitude =
+	    new Hashtable<Integer, Double>(50);
+
+    public static double getLongditude(int i) {
+	return longditude.get(i);
+    }
+
+    public static double getLatitude(int i) {
+	return latitude.get(i);
+    }
+
+    //Method for writing GPS coordinates from external thread
+    public static void coordinateHandling(double lat, double lon, int i) {
+	latitude.put(i, lat);
+	longditude.put(i, lon);
+    }
+
+    public static void addGps() {
+	latitude.put(gpsNr, 0.0);
+	gpsNr++;
+    }
+
+    public static void removeGps(int i) {
+	latitude.remove(i);
+	if (gpsNr > 0) {
+	    gpsNr--;
+	}
+    }
+
+    public static int getGpsMap() {
+	return gpsNr;
+    }
+}
